7b86c94415799bf0aea0a45fd945af6995895960
yashbhatia100 <bhatia.yash100@gmail.com>
2021-04-28 12:23:26 +0530

    updating

--- onlineplantnursery/src/service/customerService.js
+++ onlineplantnursery/src/service/customerService.js
@@ -1,43 +1,26 @@
 import axios from "axios";
 const baseUrl = "http://localhost:8585";
 
-function addCustomer(data){
-    const url = baseUrl + "/customer/add";
-    let requestData = {
-        
-        customerName : data.customerName,
-        customerEmail : data.customerEmail,
-        username : data.username,
-        password : data.password,
-        houseNo : data.houseNo,
-        colony : data.colony,
-        city : data.city,
-        state : data.state,
-        pincode : data.pincode,
-
-    };
-
-
-    console.log("Inside add Customer service");
-    const promise = axios.post(url, requestData);
-    return promise;
-
-    
+function addCustomer(data) {
+  const url = baseUrl + "/customer/add";
+  let requestData = {
+    customerName: data.customerName,
+    customerEmail: data.customerEmail,
+    username: data.username,
+    password: data.password,
+    houseNo: data.houseNo,
+    colony: data.colony,
+    city: data.city,
+    state: data.state,
+    pincode: data.pincode,
+  };
+  console.log("Inside add Customer service");
+  const promise = axios.post(url, requestData);
+  return promise;
 }
- function getCustomer(id){
-    const url = baseUrl + "/customer/fetch/byid/" + id  ;
-    const promise = axios.get(url);
-    return promise;
+function getCustomer() {
+  const url = baseUrl + "/customer/get";
+  const promise = axios.get(url);
+  return promise;
 }
-
-function updateCustomerName(data){
-    const url= baseUrl + "/customer/changename" ;
-    let requestData={
-        id: data.id,
-        customerName: data.customerName
-    };
-    const promise = axios.put(url, requestData);
-    return promise;
-
-}
-export {addCustomer,getCustomer, updateCustomerName};
+export { addCustomer, getCustomer };
